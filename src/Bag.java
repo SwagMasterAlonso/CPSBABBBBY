4c934f7f61bded0b4a2fc297fe21483886f76026
alonso_martinez <almartnz@gmail.com>
2016-02-16 20:15:56 -0500

    Bag and Item

---
+++ src/Bag.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+Alonso
+*/
+public class Bag {
+
+	
+	String name;
+	int weight;
+	List<Item> listOfItems = new ArrayList<Item>();
+
+	
+	
+	public Bag(String nameOfBag, int weightOfBag,List<Item> itemList){
+		this.name = name;
+		this.weight = weightOfBag;
+		this.listOfItems = itemList;
+		
+	}
+			
+}
--- src/Item.java
+++ src/Item.java
@@ -3,4 +3,14 @@ Alonso
 */
 public class Item {
 
+	
+	String name;
+	
+	int weight;
+	
+	public Item(String nameItem, int weightItem){
+		this.name = nameItem;
+		
+		this.weight = weightItem;
+	}
 }
