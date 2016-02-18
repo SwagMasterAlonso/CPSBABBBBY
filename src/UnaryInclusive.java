94dc313be3196c0c3e22778108ab7bfd7e3a67e8
alonso_martinez <almartnz@gmail.com>
2016-02-17 21:26:44 -0500

    Unary class

---
+++ src/UnaryInclusive.java
@@ -0,0 +1,22 @@
+/**
+Alonso
+*/
+public class UnaryInclusive {
+
+
+	
+	Item itemObj;
+	
+	public UnaryInclusive(Item item){
+		this.itemObj = item;
+	}
+	
+//	
+//	public Boolean checkConstraint(){
+//		if(this.itemObj.)
+//		
+//	}
+	
+	
+	
+}
